4bfa70672b48a7a4ae56f9c54214e685732bb959
Christian Cabrejos <piporuno@gmail.com>
2015-03-02 09:15:05 -0800

    Update PurchasesList.h
    added method to add purchase from file

--- PurchasesList.h
+++ PurchasesList.h
@@ -1,9 +1,14 @@
-/*
- * PurchasesList.h
- *
- *  Created on: Feb 20, 2015
- *      Author: piporuno
- */
+/**************************************************************************
+*        __          PROGRAMMED BY       : Jinyoung Ko, Nicole Montecillo
+*        \_}                             : Augusto Cabrejos, Andrew Gadbois
+*       .-./--.      STUDENT ID          : 589855, 2
+*     /# ^^     \                        : 3, 4
+*    |           |   CLASS               : CS1C
+*     \         /    SECTION             : TTH 12:30-4:20PM
+*      \       /     CLASS PROJECT #1    : Bulk Club
+*       `"`"`"`
+*     TEAM APPLE
+**************************************************************************/
 
 #ifndef PURCHASESLIST_H_
 #define PURCHASESLIST_H_
@@ -13,10 +18,12 @@
 class PurchasesList
 {
 	public:
+		//CONSTRUCTORS & DESTRUCTOR
 		PurchasesList();
 		virtual ~PurchasesList();
 
 		//MUTATORS
+		void AddPurchaseFromFile(string inputFileName);
 		void AddPurchase(Purchase* newPurchase);
 
 		//ACCESORS
