43a53edacc5a916c967de5b8648a93000e1467cc
Christian Cabrejos <piporuno@gmail.com>
2015-03-04 16:33:42 -0800

    Update BulkClub.cpp
    added Purchasesbymember method implementation

--- BulkClub.cpp
+++ BulkClub.cpp
@@ -70,3 +70,99 @@ void BulkClub::CheckExpMembers(MemberList& list) //list to search
 	}
 	cout << endl;
 }
+
+void BulkClub::PurchasesbyMember(MemberList& list,
+								 PurchasesList& purchases)
+{
+	//VARIABLE DECLARATION
+	string memberName;				//IN - the member name to search
+	int	   memberNumber;			//IN - the membership number to search
+	int	   selection;				//IN - user's selection from menu
+	Member *searchResult;			//IN - the member found from search
+	PurchasesList purchaseByMember;	//IN - list of purchases by member
+
+	//PROCESSING - DO-WHILE - Used to prompt user to select search by name
+	//			   or number. Exits once a valid selection is input
+	do
+	{
+		cout << "Sales report: of a specific member by:" << endl
+			 << "1 - Membership Number" << endl
+			 << "2 - Member Name" << endl
+			 << "Enter a command: ";
+
+		selection = GetAndCheckInt(1, 2);
+		cout << endl;
+	}while(selection == -1);
+
+	//PROCESSING - SWITCH - Used to call different functions bases on user
+	//			   selection
+	switch(selection)
+	{
+		case 1:	//Searches purchases by membership number
+				//PROCESSING - DO-WHILE - Used to prompt user for a 
+				//			   memberhsip number.
+				do
+				{
+					//INPUT - prompts user for membership number
+					cout << "Enter Membership Number: ";
+					memberNumber = GetAndCheckInt(1, 99999);
+					cout << endl;
+				}while(memberNumber == -1);
+				
+				//SearchForMember - Searches list for member number
+				searchResult = list.SearchForMember(memberNumber);
+				
+				//PROCESSING - IF-THEN-ELSE - check if membership was found
+				if(searchResult == NULL)
+				{
+					//Output - Not found message for membership not found
+					cout << "Membership #" << memberNumber << " was not "
+						 <<	"found" << endl << endl;
+				}
+				else
+				{
+					//Output - Found message for when member was found
+					cout << "Purchases Report for membership #"
+						 << memberNumber << ": " << endl;
+					
+					//FindPurchasesByMember - finds purchases by member
+					purchases.FindPurchasesByMember(purchaseByMember,
+													memberNumber);
+					
+					//DisplayPurchasesList - displays list of purchases
+					purchaseByMember.DisplayPurchasesList();
+				}
+				break;
+		case 2: //Searches purchases by member name
+				//INPUT - prompts user for member name
+				cout << "Enter Member Name: ";
+				cin.ignore(1000, '\n');
+				getline(cin, memberName);
+				
+				//SearchForMember - searches list for member name
+				searchResult = list.SearchForMember(memberName);
+				cout << endl;
+
+				//PROCESSING - IF-THEN-ESLE - Checks if list is empty
+				if(searchResult == NULL)
+				{
+					//OUTPUT - not found message for no member found
+					cout << "Member " << memberName << " was not found"
+						 << endl << endl;
+				}
+				else
+				{
+					//OUTPUT - found message when member was found
+					cout << "Purchases Report for member " << memberName
+						 << ": " << endl;
+					
+					//PurchasesByMember - finds purchases by member
+					purchases.FindPurchasesByMember(purchaseByMember,
+													searchResult->GetMemberNumber());
+					
+					//DisplayPurchaseList- displays list of purchases
+					purchaseByMember.DisplayPurchasesList();
+				}
+				break;
+	}
+}
