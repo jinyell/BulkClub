4037c27301d5c5353c463580dd18bd3711a6196e
Augusto Christian Cabrejos <piporuno@gmail.com>
2015-03-15 03:40:07 -0700

    Update Product.h

--- Product.h
+++ Product.h
@@ -35,20 +35,21 @@ class Product
 		Product(string itemName,
 				float  intemPrice,
 				int    itemQtySold);
+		Product(Product const &someProduct);
 		virtual ~Product();
 
 		//MUTATORS
-		void SetNext(Product* nextNode);			//Sets next pointer
-		void SetPrevious(Product* previousNode);	//Sets previous pointer
-		void SetProductQtySold(int saleQty);		//Sets product qty
+		void SetNext(Product* nextNode);	//Sets next pointer
+		void SetPrev(Product* prevNode);	//Sets previous pointer
+		void SetQtySold(int saleQty);		//Sets product qty
 
 		//ACCESSORS
-		string 	 GetProductName() const;		//Gets product name
-		float  	 GetProductPrice() const;		//Gets product price
-		int		 GetProductQtySold() const;		//Gets qty sold
-		Product* GetNext() const;				//Gets next pointer
-		Product* GetPrevious() const;			//Gets previous pointer
-		void PrintProductInfo() const;			//Prints single product
+		string 	 GetName() const;			//Gets product name
+		float  	 GetPrice() const;			//Gets product price
+		int		 GetQtySold() const;		//Gets qty sold
+		Product* GetNext() const;			//Gets next pointer
+		Product* GetPrev() const;			//Gets previous pointer
+		void 	 PrintProductInfo() const;	//Prints single product
 
 	private:
 		string  productName;		//Product name
